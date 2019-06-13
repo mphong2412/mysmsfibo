94cfd0fb1ce0128c406c59a67158c88c3ef5cede
phong2412 <minhphong241297@gmail.com>
2019-06-13 21:47:03 +0700

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -288,7 +288,7 @@
             </div>
         </div>
     </div>
-
+</div>  
     <footer class="sticky-footer bg-white">
         <div class="container my-auto">
             <div class="copyright text-center my-auto">
